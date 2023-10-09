765bb0106ed139aaf1d3654af501f04ecee07b22
Adina Karantza <adina.elisheva@gmail.com>
2023-10-09 13:46:26 +0000

    shifting calendar

--- constants.js
+++ constants.js
@@ -27,8 +27,7 @@ const TOGGLE = 3;
 
 const DAY_MILLIS = 1000*60*60*24;
 
-const DAYS_OF_WEEK_INITIALS = ['M', 'T', 'W', 'R', 'F', 'S', 'U'];
-const DAYS_OF_WEEK_ABBREV = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const DAYS_OF_WEEK_ABBREV = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 // Used to ensure a date w/no time is converted to midnight on the correct date
 const TIME_SUFFIX = 'T00:00:00.000';
