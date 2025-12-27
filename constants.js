c975fec03d237e7f0ae67cd3c9edca2bf448a004
Adina Karantza <adina.elisheva@gmail.com>
2025-12-27 16:58:54 +0000

    changing latenight to be identical to prev day

--- constants.js
+++ constants.js
@@ -13,6 +13,9 @@ const DAYS_OF_WEEK_ABBREV = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 // Used to ensure a date w/no time is converted to midnight on the correct date
 const TIME_SUFFIX = 'T00:00:00.000';
 
-const NOW = new Date();
+let NOW = new Date();
 const IS_EARLY_HOURS = NOW.getHours() <= 4;
+if (IS_EARLY_HOURS) {
+  NOW = new Date(NOW.getTime() - DAY_MILLIS);
+}
 let BASE_DATE, DATE_INPUT;
